9599f25c3a0794b7cf207ab8c9041a193f55ef92
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-08-27 10:07:28 -0700

    updated readme wit tools, app description, and how to play

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# mega-game
+# Mega-Game
+
+A little rpg (Mega Man themed)
+
+## How to play
+
+- Each character has different stats some are known (i.e. hit points) others are secret (att/def)
+- Player will choose 1 of 4 characters to be their hero.
+- The unchosen characters become enemies.
+- The Player will then choose their first oppenent.
+- The player will attack until the opponent or enemy is defeated.
+- The player will then choose the next opponentand repeat until the player or all opponents have been defeated.
+- Player hit points will not reset between battles.
+- The goal is to find a path through all opponents
+
+## Tools
+
+- HTML
+- CSS
+- Javascript
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,72 +1,58 @@
 //start/reset game
-$(document).ready(function (){
+$(document).ready(function() {
+	//stats assigned to characters
+	let att = 6;
+	let newAttack = 6;
+	let counterDmg = 0;
+	let hp1 = 120;
+	let hp2 = 180;
+	let hp3 = 100;
+	let hp4 = 150;
 
-//stats assigned to characters
-let att = 6;
-let newAttack = 6;
-let counterDmg = 0;
-let hp1 = 120;
-let hp2 = 180;
-let hp3 = 100;
-let hp4 = 150;
+	charcterlist: [
+		{
+			name: 'Mega-man',
+			visual: 'assets/images/mega1.png',
+			hp: 120,
+			att: 10,
+			counterDmg: 20,
+		},
+		{
+			name: 'Proto-Man',
+			visual: 'assets/images/proto2',
+			hp: 180,
+			att: 10,
+			counterDmg: 20,
+		},
+		{
+			name: 'Roll',
+			visual: 'assets/images/roll1.png',
+			hp: 100,
+			att: 10,
+			counterDmg: 20,
+		},
+		{
+			name: 'Dr. Wily',
+			visual: 'assets/images/wily1.png',
+			hp: 150,
+			att: 10,
+			counterDmg: 20,
+		},
+	];
 
+	//user choose character
+	$('.char1').on('click', function() {
+		//userCharacter moves to user position (this doesnt work)
+		$('attacker').append($('char1 >div'));
+	});
 
-charcterlist: [
-    {
-        name: 'Mega-man',
-	    visual: 'assets/images/mega1.png',
-	    hp: 120, 
-	    att: 10,
-	    counterDmg: 20,
-    },
-    {
-        name: 'Proto-Man',
-	    visual: 'assets/images/proto2',
-	    hp: 180, 
-	    att: 10,
-	    counterDmg: 20,
-    },
-    {
-        name: 'Roll',
-	    visual: 'assets/images/roll1.png',
-	    hp: 100, 
-	    att: 10,
-	    counterDmg: 20,
-    },
-    {
-        name: 'Dr. Wily',
-	    visual: 'assets/images/wily1.png',
-	    hp: 150, 
-	    att: 10,
-	    counterDmg: 20,
-    }
-]
+	//all others become enemies
 
-//if time allows find a way to allow the computer to chose from multiple images and place those in the 4 user choices to create "more" character options at "random"
+	//user chooses enemy to fight
 
+	//chosen enemy moves to defender position
 
-//user choose character
-$('.char1').on('click', function () {
-    //userCharacter moves to user position (this doesnt work)
-    $('attacker').append( $('char1 >div') );
+	//attack button function on click damages enemy removing their hp click (should do nothing before all characters are chosen), if user wins remove enemy and allow user to select new enemy, if user has defeated all enemies display "WIN!" counter attack function. Triggers after enemy takes damage, removing users hp before user can click attack again. If user hp drops to 0 or below display "YOU LOSE!" else attack button function plus base
+
+	//target reset button to rest game back to default
 });
-
-//all others become enemies
-
-//user chooses enemy to fight
-
-//chosen enemy moves to defender position
-
- //attack button function on click damages enemy removing their hp click (should do nothing before all characters are chosen), if user wins remove enemy and allow user to select new enemy, if user has defeated all enemies display "WIN!" counter attack function. Triggers after enemy takes damage, removing users hp before user can click attack again. If user hp drops to 0 or below display "YOU LOSE!" else attack button function plus base
-
-    // $('.att').on('click', function(){
-        
-    //     let newAttack += attack;
-
-    // })
-
-
-
-//target reset button to rest game back to default
-
-})
